25479f86225c972aa8e90a1e1c87b3eb7ca2041e
Gabriel Godoy <gabyreload@gmail.com>
2021-09-25 19:35:08 -0300

    Install EJS and replace html files for ejs

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "mario", snippet: "ksdnflkasmgmmfgfg" },
+    { title: "luigi", snippet: "pokpsdf6456465afg" },
+    { title: "princes", snippet: "645665gd1h5f16gj" },
+  ];
+
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
