5bca61b90868f53fd9a9fc023bc6a59a5bd3814a
Gabriel Godoy <gabyreload@gmail.com>
2021-09-26 10:09:00 -0300

    add blogControllers.js and blogRoutes.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+
+const blogRoutes = require("./routes/blogRoutes");
 
 //express app
 const app = express();
@@ -34,35 +35,10 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
 });
 
-//blog Routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//blogs Routes
+app.use("/blogs", blogRoutes);
 
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
+//404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
